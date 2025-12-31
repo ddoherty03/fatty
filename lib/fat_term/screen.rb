578bf6d9937921d5dd7066e2cdbf973bcee786b7
Daniel E. Doherty <ded@ddoherty.net>
2025-12-31 16:26:14 -0600

    Modify screen for Terminal; use KeyMap::MAP for dispatch.

--- lib/fat_term/screen.rb
+++ lib/fat_term/screen.rb
@@ -5,6 +5,7 @@ require "curses"
 module FatTerm
   class Screen
     attr_reader :rows, :cols, :output_win, :input_win
+    attr_accessor :diagnostic_sink
 
     def initialize
       Curses.init_screen
@@ -35,16 +36,21 @@ module FatTerm
       Curses.close_screen
     end
 
-    def read_key
+    def read_key(debug: false)
       ch = @input_win.getch
-
+      if debug
+        diagnostic_sink.call("getch => #{ch.inspect} (#{ch.class}) #{KeyEvent::KEY_SYM_MAP[ch]}")
+      end
       case ch
       when Integer
         case ch
-        when 3
-          KeyEvent.new(key: :c, ctrl: true)
-        when 10
-          KeyEvent.new(key: :enter)
+        when 8, 127, Curses::KEY_BACKSPACE then KeyEvent.new(key: :backspace, ctrl: false)
+        when 0..26
+          # Ctrl-@ (0) through Ctrl-Z (26)
+          letter = (ch + 96).chr
+          KeyEvent.new(key: letter.to_sym, ctrl: true)
+        when 27
+          read_meta_key
         when Curses::KEY_LEFT
           KeyEvent.new(key: :left)
         when Curses::KEY_RIGHT
@@ -53,13 +59,29 @@ module FatTerm
           KeyEvent.new(key: :up)
         when Curses::KEY_DOWN
           KeyEvent.new(key: :down)
-        when Curses::KEY_BACKSPACE, 127
-          KeyEvent.new(key: :backspace)
         when Curses::KEY_RESIZE
           KeyEvent.new(key: :resize)
+        else
+          if KeyMap::MAP[ch]
+            KeyMap::MAP[ch]
+          end
         end
       when String
-        KeyEvent.new(key: ch)
+        case ch
+        when "\e"
+          read_meta_key
+        else
+          KeyEvent.new(key: ch.to_sym, text: ch)
+        end
+      end
+    end
+
+    def read_meta_key
+      nxt = @input_win.getch
+      return unless nxt
+
+      if nxt.is_a?(String) && nxt.length == 1
+        KeyEvent.new(key:  nxt.to_sym, text: nxt, meta: true)
       end
     end
   end
