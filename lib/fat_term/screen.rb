4c2cafdb0786de609ea82e2a2b5a37401ae5c06c
Daniel E. Doherty <ded@ddoherty.net>
2026-01-01 06:24:44 -0600

    Re-write Screen#read_key to use CURSES_TO_EVENT map.

--- lib/fat_term/screen.rb
+++ lib/fat_term/screen.rb
@@ -36,43 +36,66 @@ module FatTerm
       Curses.close_screen
     end
 
+    # def read_key(debug: false)
+    #   ch = @input_win.getch
+    #   if debug
+    #     diagnostic_sink.call("getch => #{ch.inspect} (#{ch.class}) #{KeyEvent::KEY_SYM_MAP[ch]}")
+    #   end
+    #   case ch
+    #   when Integer
+    #     case ch
+    #     when 8, 127, Curses::KEY_BACKSPACE then KeyEvent.new(key: :backspace, ctrl: false)
+    #     when 0..26
+    #       # Ctrl-@ (0) through Ctrl-Z (26)
+    #       letter = (ch + 96).chr
+    #       KeyEvent.new(key: letter.to_sym, ctrl: true)
+    #     when 27
+    #       read_meta_key
+    #     when Curses::KEY_LEFT
+    #       KeyEvent.new(key: :left)
+    #     when Curses::KEY_RIGHT
+    #       KeyEvent.new(key: :right)
+    #     when Curses::KEY_UP
+    #       KeyEvent.new(key: :up)
+    #     when Curses::KEY_DOWN
+    #       KeyEvent.new(key: :down)
+    #     when Curses::KEY_RESIZE
+    #       KeyEvent.new(key: :resize)
+    #     else
+    #       if KeyMap::MAP[ch]
+    #         KeyMap::MAP[ch]
+    #       end
+    #     end
+    #   when String
+    #     case ch
+    #     when "\e"
+    #       read_meta_key
+    #     else
+    #       KeyEvent.new(key: ch.to_sym, text: ch)
+    #     end
+    #   end
+    # end
+
     def read_key(debug: false)
       ch = @input_win.getch
       if debug
-        diagnostic_sink.call("getch => #{ch.inspect} (#{ch.class}) #{KeyEvent::KEY_SYM_MAP[ch]}")
+        diagnostic_sink.call("getch => #{ch.inspect} (#{ch.class}) #{CURSES_TO_EVENT[ch]}")
       end
-      case ch
-      when Integer
-        case ch
-        when 8, 127, Curses::KEY_BACKSPACE then KeyEvent.new(key: :backspace, ctrl: false)
-        when 0..26
-          # Ctrl-@ (0) through Ctrl-Z (26)
-          letter = (ch + 96).chr
-          KeyEvent.new(key: letter.to_sym, ctrl: true)
-        when 27
-          read_meta_key
-        when Curses::KEY_LEFT
-          KeyEvent.new(key: :left)
-        when Curses::KEY_RIGHT
-          KeyEvent.new(key: :right)
-        when Curses::KEY_UP
-          KeyEvent.new(key: :up)
-        when Curses::KEY_DOWN
-          KeyEvent.new(key: :down)
-        when Curses::KEY_RESIZE
-          KeyEvent.new(key: :resize)
-        else
-          if KeyMap::MAP[ch]
-            KeyMap::MAP[ch]
-          end
-        end
-      when String
-        case ch
-        when "\e"
-          read_meta_key
-        else
-          KeyEvent.new(key: ch.to_sym, text: ch)
-        end
+      if ch == 27 || ch == "\e"
+        # Handle Meta
+        read_meta_key
+      elsif CURSES_TO_EVENT.key?(ch)
+        # Known curses constants → normalized
+        CURSES_TO_EVENT[ch]
+      elsif ch.is_a?(Integer) && (0..26).include?(ch)
+        # ASCII control keys
+        KeyEvent.new(key: (ch + 96).chr.to_sym, ctrl: true)
+      elsif ch.is_a?(String)
+        # Printable characters
+        KeyEvent.new(key: ch.to_sym, text: ch)
+      else
+        # Unknown integer → preserve
+        KeyEvent.new(key: ch)
       end
     end
 
@@ -81,7 +104,9 @@ module FatTerm
       return unless nxt
 
       if nxt.is_a?(String) && nxt.length == 1
-        KeyEvent.new(key:  nxt.to_sym, text: nxt, meta: true)
+        KeyEvent.new(key: nxt.to_sym, text: nxt, meta: true)
+      else
+        KeyEvent.new(key: nxt, meta: true)
       end
     end
   end
