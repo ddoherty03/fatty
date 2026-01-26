256393cadc6cf122d44454fc5c491e76c304e6f5
Daniel E. Doherty <ded@ddoherty.net>
2026-01-26 08:22:41 -0600

    Viewport updates.

--- lib/fat_term/viewport.rb
+++ lib/fat_term/viewport.rb
@@ -2,23 +2,34 @@
 
 module FatTerm
   class Viewport
-    attr_reader :top, :height
+    attr_reader :top
+    attr_accessor :height
 
     def initialize(top: 0, height:)
-      @top = top
+      @top    = top
       @height = height
+    end
+
+    def adjust_for_trim(n)
+      @top = [@top - n, 0].max
+    end
+
+    def at_bottom?(total_lines = nil)
+      return false unless total_lines
+
+      @top >= max_top(total_lines)
     end
 
     def slice(lines)
       lines[@top, @height] || []
     end
 
-    def max_top(lines)
-      [lines.length - @height, 0].max
+    def max_top(total_lines)
+      [total_lines - @height, 0].max
     end
 
     def clamp!(lines)
-      @top = [[@top, 0].max, max_top(lines)].min
+      @top = [[@top, 0].max, max_top(lines.size)].min
     end
 
     def scroll_up(lines, n = 1)
@@ -31,16 +42,20 @@ module FatTerm
       clamp!(lines)
     end
 
+    def scroll_to_bottom(lines)
+      @top = max_top(lines.length)
+    end
+
+    def page_up(lines)
+      scroll_up(lines, @height)
+    end
+
+    def page_down(lines)
+      scroll_down(lines, @height)
+    end
+
     def reset
       @top = 0
-    end
-
-    def page_up
-      scroll_up(@height)
-    end
-
-    def page_down(max:)
-      scroll_down(@height, max:)
     end
   end
 end
