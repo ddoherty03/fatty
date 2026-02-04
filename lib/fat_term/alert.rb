41b4ca13a2849239974d129a1d64571228b12e22
Daniel E. Doherty <ded@ddoherty.net>
2026-02-04 15:58:07 -0600

    Spec and document Alert.

--- lib/fat_term/alert.rb
+++ lib/fat_term/alert.rb
@@ -2,25 +2,48 @@ module FatTerm
   class Alert
     attr_reader :level, :message, :details
 
+    # Return a new Alert object
+    #
+    # @param message [String]
+    # @param level [:info, :warning, :error]
+    # @param details [Hash|String]
+    # @param sticky [Boolean]
+    # @return [Alert]
     def initialize(message:, level: :info, details: nil, sticky: false)
       @message = message
-      @level = level.to_sym    # :info, :warning, :error
-      @details = details  # Hash or String
-      @sticky  = !!sticky   # require explicit dismiss
+      @level = level.to_sym
+      @details = details
+      @sticky  = !!sticky
     end
 
+    # Return a new Alert object at level info
+    #
+    # @param msg [String]
+    # @return [Alert] with level info
     def self.info(msg)
       new(message: msg, level: :info)
     end
 
+    # Return a new Alert object at level warning
+    #
+    # @param msg [String]
+    # @return [Alert] with level warning
     def self.warning(msg)
       new(message: msg, level: :warning)
     end
 
+    # Return a new Alert object at level error
+    #
+    # @param msg [String]
+    # @return [Alert] with level error
     def self.error(msg)
       new(message: msg, level: :error)
     end
 
+    # Return whether this Alert is sticky, meaning that it should not be
+    # cleared until a key is presses or another Alert displayed
+    #
+    # @return [true, false] is this Alert sticky?
     def sticky?
       @sticky
     end
---
+++ spec/fat_term/alert_spec.rb
@@ -0,0 +1,61 @@
+module FatTerm
+  RSpec.describe Alert do
+    it "requires the message parameter" do
+      expect { Alert.new }.to raise_error(ArgumentError)
+    end
+
+    it "initializes with just the message parameter" do
+      a = Alert.new(message: "hello, world")
+      expect(a).to be_a(Alert)
+      expect(a.message).to eq('hello, world')
+      expect(a.level).to eq(:info)
+      expect(a).not_to be_sticky
+    end
+
+    it "initializes using .info with just the message parameter" do
+      a = Alert.info("hello, world")
+      expect(a).to be_a(Alert)
+      expect(a.message).to eq('hello, world')
+      expect(a.level).to eq(:info)
+      expect(a).not_to be_sticky
+    end
+
+    it "initializes using .warning with just the message parameter" do
+      a = Alert.warning("hello, world")
+      expect(a).to be_a(Alert)
+      expect(a.message).to eq('hello, world')
+      expect(a.level).to eq(:warning)
+      expect(a).not_to be_sticky
+    end
+
+    it "initializes using .error with just the message parameter" do
+      a = Alert.error("hello, world")
+      expect(a).to be_a(Alert)
+      expect(a.message).to eq('hello, world')
+      expect(a.level).to eq(:error)
+      expect(a).not_to be_sticky
+    end
+
+    it "initializes non-sticky with all parameters" do
+      a = Alert.new(
+        message: "hello, world",
+        level: :debug,
+        details: { one: "1", two: "2" },
+      )
+      expect(a).to be_a(Alert)
+      expect(a.message).to eq('hello, world')
+      expect(a.level).to eq(:debug)
+      expect(a.details).to be_a(Hash)
+      expect(a).not_to be_sticky
+    end
+
+    it "initializes sticky with all parameters" do
+      a = Alert.new(message: "hello, world", level: :debug, details: {one: "1", two: "2"}, sticky: true)
+      expect(a).to be_a(Alert)
+      expect(a.level).to eq(:debug)
+      expect(a.details).to be_a(Hash)
+      expect(a).to be_sticky
+    end
+
+  end
+end
