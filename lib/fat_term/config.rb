ed88a4a4b589bf644ebae63b4b5f9cb52851272f
Daniel E. Doherty <ded@ddoherty.net>
2026-02-05 06:47:34 -0600

    Refactor Config to read verbosely on a config error.

--- lib/fat_term/config.rb
+++ lib/fat_term/config.rb
@@ -2,19 +2,18 @@
 
 module FatTerm
   module Config
-    # The @errors variable provides a way to accummulate error messages so
-    # that they can be displayed to the user once the Terminal and Alert panel
-    # are up and running, which is probably not the case when the configs are
-    # being read in.
-
-    # Return an Array of errors seen while reading in the config files.
-    def self.errors
-      @errors ||= []
-    end
-
-    # Clear the Array of errors.
-    def self.clear_errors!
-      errors.clear
+    # Read in the general configuration for fat_term in config.yml for certain
+    # user-adjustable features of fat_term.  One of these is the logging,
+    # including the location of the log file.
+    def self.config
+      FatConfig::Reader.new('fat_term').read('config')
+    rescue FatConfig::ParseError => ex
+      begin
+        FatConfig::Reader.new("fat_term").read("config", verbose: true)
+      rescue StandardError
+        # ignore secondary failure; we only wanted diagnostic output
+      end
+      raise ex
     end
 
     # Read in the keydefs.yml config file that maps numeric keycodes returned
@@ -22,8 +21,12 @@ module FatTerm
     def self.keydefs
       FatConfig::Reader.new('fat_term').read('keydefs')
     rescue FatConfig::ParseError => ex
-      record_error("keydefs", ex)
-      {}
+      begin
+        FatConfig::Reader.new('fat_term').read('keydefs', verbose: true)
+      rescue StandardError
+        # ignore secondary failure; we only wanted diagnostic output
+      end
+      raise ex
     end
 
     # Read in the keybindings.yml config file that maps key names (together
@@ -36,26 +39,12 @@ module FatTerm
       bindings = kb_cfg[:keybindings]
       Array(bindings)
     rescue FatConfig::ParseError => ex
-      record_error("keybindings", ex)
-      []
-    end
-
-    # Read in the general configuration for fat_term in config.yml for certain
-    # user-adjustable features of fat_term.  One of these is the logging,
-    # including the location of the log file.
-    def self.config
-      FatConfig::Reader.new('fat_term').read('config')
-    rescue FatConfig::ParseError => ex
-      record_error("config", ex)
-      {}
-    end
-
-    def self.record_error(file, ex)
-      msg = "Config parse error in #{file}.yml: #{ex.message}"
-      errors << { file: file, error: ex }
-
-      FatTerm.log("config_parse_error", level: :error, tag: :config, file: "#{file}.yml", err: ex.class.name, msg: ex.message)
-      msg
+      begin
+        FatConfig::Reader.new("fat_term").read("keybindings", verbose: true)
+      rescue StandardError
+        # ignore secondary failure; we only wanted diagnostic output
+      end
+      raise ex
     end
   end
 end
