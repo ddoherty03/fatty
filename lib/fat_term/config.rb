151e77c54db67069a29f675285bbd0bfffa9ddf2
Daniel E. Doherty <ded@ddoherty.net>
2026-02-09 09:14:07 -0600

    Make progname a class instance variable of FatConfig and use it.
    This give the consumer of the library the ability to have its config files and
    log files located in a place that makes sense for their program.

--- lib/fat_term/config.rb
+++ lib/fat_term/config.rb
@@ -2,24 +2,29 @@
 
 module FatTerm
   module Config
+    @progname = 'fat_term'
+    class << self
+      attr_accessor :progname
+    end
+
     # Read in the general configuration for fat_term in config.yml for certain
     # user-adjustable features of fat_term.  One of these is the logging,
     # including the location of the log file.
     def self.config
-      FatConfig::Reader.new('fat_term').read('config')
+      FatConfig::Reader.new(progname).read('config')
     end
 
     # Read in the keydefs.yml config file that maps numeric keycodes returned
     # by curses but not assigned a key name.
     def self.keydefs
-      FatConfig::Reader.new('fat_term').read('keydefs')
+      FatConfig::Reader.new(progname).read('keydefs')
     end
 
     # Read in the keybindings.yml config file that maps key names (together
     # with any modifiers, shift, ctrl, meta) to action names to be triggered
     # by the key chord.
     def self.keybindings
-      kb_cfg = FatConfig::Reader.new("fat_term").read("keybindings")
+      kb_cfg = FatConfig::Reader.new(progname).read("keybindings")
       return [] unless kb_cfg.is_a?(Hash)
 
       bindings = kb_cfg[:keybindings]
