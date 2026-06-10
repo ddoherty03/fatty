f98723ff9fdc1f4b646e17041286d84cb967ebe5
Daniel E. Doherty <ded@ddoherty.net>
2026-06-10 05:45:05 -0500

    Eliminate View class as too thin.
    We just moved their functionality into Session#view.

--- lib/fatty/view/input_view.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# module Fatty
-#   class InputView < Fatty::View
-#     def draw(renderer:, terminal:, session:)
-#       renderer.render_input_field(session.field)
-#     end
-#   end
-# end
--- lib/fatty/view/output_view.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# module Fatty
-#   class OutputView < View
-#     def draw(renderer:, terminal:, session:)
-#       renderer.render_output(session.output, viewport: session.viewport)
-#     end
-#   end
-# end
