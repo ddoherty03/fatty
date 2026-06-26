88e8d1ff4b3be95b1a533e3a6e8cdb3c47be7069
Daniel E. Doherty <ded@ddoherty.net>
2026-06-26 10:19:48 -0500

    Fix directories used in keybinding and keydef configs.

--- lib/fatty/config.rb
+++ lib/fatty/config.rb
@@ -31,46 +31,36 @@ module Fatty
     end
 
     def self.user_keydefs_path
-      @reader ||= FatConfig::Reader.new(progname)
-      reader.config_paths('keydefs')[:user].first || default_user_path('keydefs')
+      @reader ||= FatConfig::Reader.new(progname, user_dir: dir)
+      reader.config_paths("keydefs")[:user].first || default_user_path("keydefs")
     end
 
     def self.user_keybindings_path
-      self.reader ||= FatConfig::Reader.new(progname)
-      reader.config_paths('keybindings')[:user].first || default_user_path('keybindings')
+      self.reader ||= FatConfig::Reader.new(progname, user_dir: dir)
+      reader.config_paths("keybindings")[:user].first || default_user_path("keybindings")
     end
 
     # Read in the keydefs.yml config file that maps numeric keycodes returned
     # by curses but not assigned a key name.
     def self.keydefs
-      FatConfig::Reader.new(progname).read('keydefs')
+      FatConfig::Reader.new(progname, user_dir: dir).read("keydefs")
     end
 
     # Read in the keybindings.yml config file that maps key names (together
     # with any modifiers, shift, ctrl, meta) to action names to be triggered
     # by the key chord.
     def self.keybindings
-      kb_cfg = FatConfig::Reader.new(progname).read("keybindings")
+      kb_cfg =
+        FatConfig::Reader
+          .new(progname, user_dir: dir)
+          .read("keybindings")
       return [] unless kb_cfg.is_a?(Hash)
 
-      bindings = kb_cfg[:keybindings]
-      Array(bindings)
+      Array(kb_cfg[:keybindings])
     end
 
     def self.default_user_path(name = 'config')
       "~/.config/#{progname}/#{name}.yml"
-    end
-
-    def self.sys_themes_dir
-      File.expand_path("config_files/themes", __dir__)
-    end
-
-    def self.user_config_dir
-      dir || File.join(ENV.fetch("XDG_CONFIG_HOME", File.expand_path("~/.config")), progname)
-    end
-
-    def self.user_themes_dir
-      File.join(user_config_dir, "themes")
     end
 
     def self.default_config_dir
